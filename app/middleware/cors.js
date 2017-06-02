2def856bc617e40d6566dccce3599df73f1020c7
Dmitry Fontanov <dfontanov@odin.com>
2017-06-02 18:34:37 +0300

    better cors

--- app/middleware/cors.js
+++ app/middleware/cors.js
@@ -1,5 +1,8 @@
 function cors(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   next();
 }
